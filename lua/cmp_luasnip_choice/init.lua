685db6568c38315cca4c89e582c03b3fa7179be5
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-07-20 13:02:54 -0400

    Add stylua config

--- lua/cmp_luasnip_choice/init.lua
+++ lua/cmp_luasnip_choice/init.lua
@@ -1,89 +1,89 @@
 local cmp = require('cmp')
 
 local default_config = {
-    auto_open = true,
-    jump_to_next = true,
+  auto_open = true,
+  jump_to_next = true,
 }
 
 local M = {
-    source = {}
+  source = {},
 }
 
 M.config = {}
 
 function M.setup(config)
-    M.config = vim.tbl_deep_extend('keep', config or {}, default_config)
+  M.config = vim.tbl_deep_extend('keep', config or {}, default_config)
 
-    if M.config.auto_open then
-        vim.api.nvim_create_autocmd('User', {
-            pattern = 'LuasnipChoiceNodeEnter',
-            callback = function()
-                vim.defer_fn(function()
-                    cmp.complete({
-                        config = {
-                            sources = {
-                                {name = 'luasnip_choice'}
-                            },
-                        },
-                    })
-                end, 100)
-            end,
-        })
-    end
+  if M.config.auto_open then
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'LuasnipChoiceNodeEnter',
+      callback = function()
+        vim.defer_fn(function()
+          cmp.complete({
+            config = {
+              sources = {
+                { name = 'luasnip_choice' },
+              },
+            },
+          })
+        end, 100)
+      end,
+    })
+  end
 end
 
 function M.source.new()
-    return setmetatable({}, {__index = M.source})
+  return setmetatable({}, { __index = M.source })
 end
 
 function M.source:is_available()
-    return require("luasnip").choice_active()
+  return require('luasnip').choice_active()
 end
 
 function M.source:execute(completion_item, callback)
-    local session = require("luasnip").session
-    local selected = session.active_choice_nodes[vim.api.nvim_get_current_buf()].choices[completion_item.index]
+  local session = require('luasnip').session
+  local selected = session.active_choice_nodes[vim.api.nvim_get_current_buf()].choices[completion_item.index]
 
-    require('luasnip').set_choice(completion_item.index)
+  require('luasnip').set_choice(completion_item.index)
 
-    -- If nested snippet node (sn) has insert node, then we do not jump otherwise it will skip the first insert position.
-    if M.config.jump_to_next and require("luasnip").jumpable(1) then
-        local has_insert = selected.insert_nodes and #selected.insert_nodes > 0 or false
-        local is_empty = selected.nodes and #selected.nodes == 1 and selected.nodes[1].static_text[1] == ""
-        if not has_insert and not is_empty then
-            require('luasnip').jump(1)
-        end
+  -- If nested snippet node (sn) has insert node, then we do not jump otherwise it will skip the first insert position.
+  if M.config.jump_to_next and require('luasnip').jumpable(1) then
+    local has_insert = selected.insert_nodes and #selected.insert_nodes > 0 or false
+    local is_empty = selected.nodes and #selected.nodes == 1 and selected.nodes[1].static_text[1] == ''
+    if not has_insert and not is_empty then
+      require('luasnip').jump(1)
     end
-    callback(completion_item)
+  end
+  callback(completion_item)
 end
 
 function M.source:get_keyword_pattern()
-    return ''
+  return ''
 end
 
 function M.source:complete(_, callback)
-    local items = {}
+  local items = {}
 
-    local choices_ok, choice_docstrings = pcall(require('luasnip').get_current_choices)
-    if not choices_ok then
-        -- no choice active: return no completion-items.
-        callback(items)
-    end
-
-    for _, choice_docstring in ipairs(choice_docstrings) do
-        table.insert(items, {
-            label = choice_docstring,
-            word = "",
-            index = _,
-            documentation = choice_docstring,
-            kind = cmp.lsp.CompletionItemKind.Text,
-        })
-    end
+  local choices_ok, choice_docstrings = pcall(require('luasnip').get_current_choices)
+  if not choices_ok then
+    -- no choice active: return no completion-items.
     callback(items)
+  end
+
+  for _, choice_docstring in ipairs(choice_docstrings) do
+    table.insert(items, {
+      label = choice_docstring,
+      word = '',
+      index = _,
+      documentation = choice_docstring,
+      kind = cmp.lsp.CompletionItemKind.Text,
+    })
+  end
+  callback(items)
 end
 
 function M.source:get_debug_name()
-    return 'luasnip_choice'
+  return 'luasnip_choice'
 end
 
 return M
